dfc1bc4616ee2c9ed9001836d10b44afd2730c89
Nazar Brych <nazar.brych.06@gmail.com>
2023-01-23 20:08:54 +0200

    LINQ expression was successfully writte

---
+++ Linq/Tasks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Linq.Objects;
+
+namespace Linq
+{
+    public static class Tasks
+    {
+
+        public static IEnumerable<YearSchoolStat> Task1(IEnumerable<Entrant> nameList, IEnumerable<int> yearList)
+        {
+            return yearList.GroupJoin(yearList.SelectMany(x => nameList.Where(n => n.Year == x)
+            .GroupBy(x => x.Year, x => x.SchoolNumber, (y, s) => s.Distinct().Count()), (year, number) => new
+            {
+                Year = year,
+                CountOfSchool = number
+            }), y => y, n => n.Year, (year, number) => new YearSchoolStat
+            {
+                Year = year,
+                NumberOfSchools = number.Select(x => x.CountOfSchool).SingleOrDefault()
+            }).OrderBy(x => x.NumberOfSchools).ThenBy(x => x.Year);
+        }
+
+    }
+}
